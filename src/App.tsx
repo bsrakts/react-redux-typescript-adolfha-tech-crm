5c5a21e3cf693d75c109f0350cd5dc56a7415c1a
Büşra Aktaş <diba.aktas@ogr.sakarya.edu.tr>
2023-09-29 00:37:15 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./features/counter/Counter";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
